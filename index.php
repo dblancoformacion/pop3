f7e8c85ad8fd78bb376f2e474d51280de0ccbf94
dblancoformacion <dblancoformacion@gmail.com>
2019-09-07 14:32:19 +0200

    Nuevos servidores

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 include 'config.php';
-
-echo 'hola mundo';
+// activar imap en php.ini
 
 $authhost="{imap.gmail.com:993/imap/ssl/novalidate-cert}";
+$authhost="{imap.gmx.net:993/imap/ssl/novalidate-cert}INBOX";
+$authhost="{imap.strato.de:993/imap/ssl/novalidate-cert}INBOX";
 
 if ($mbox=imap_open( $authhost, $user, $pass )){
 	echo "<h1>Connected</h1>\n";
@@ -12,6 +13,7 @@ if ($mbox=imap_open( $authhost, $user, $pass )){
 	for($i=imap_num_msg($mbox);$i>0;$i--){
 		$a=imap_fetchheader ( $mbox , $i);
 		$a=iconv_mime_decode_headers($a);
+		echo '<h2>'.$i.'</h2>';
 		echo '<pre>';
 		print_r($a);
 		echo '</pre>';
